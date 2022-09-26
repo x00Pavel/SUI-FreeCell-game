52d026d51028c30378a0fb10e96de3aadb964b55
x00Pavel <pavel.yadlouski@gmail.com>
2022-09-26 22:48:15 +0200

    BFS in some working state

--- sui-solution.cc
+++ sui-solution.cc
@@ -6,12 +6,12 @@
 
 std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_state) {
 	std::queue<std::shared_ptr<SearchState>> q;
-	std::map<std::shared_ptr<SearchState>, std::shared_ptr<SearchState>> parent;
-	std::map<std::shared_ptr<SearchState>, SearchAction> actions;
+	std::map<SearchState, std::shared_ptr<SearchState>> parent;
+	std::map<SearchState, SearchAction> actions;
 
 	std::shared_ptr<SearchState> shared_init_state = std::make_shared<SearchState>(init_state);
 	q.push(shared_init_state);
-	parent[shared_init_state] = std::make_shared<SearchState>(init_state); 
+	parent[init_state] = std::make_shared<SearchState>(init_state); 
 	
 	while (!q.empty()) {
 		std::shared_ptr<SearchState> current_state = q.front();
@@ -19,21 +19,23 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
 
 		for (auto action : current_state->actions()) {
 			std::shared_ptr<SearchState> new_state = std::make_shared<SearchState>(action.execute(*current_state));
+			// If the key is not present, find returns an iterator to end
+			if (parent.find(*new_state) == parent.end()) {
+				q.push(new_state);
+				parent[*new_state] = current_state;
+				actions.insert(std::pair<SearchState, SearchAction>(*new_state, action));
+			}
+			
 			if (new_state->isFinal()) {
-				std::cout << "Found final state" << std::endl;
 				std::vector<SearchAction> solution;
-				while (*new_state < *shared_init_state) {
-					solution.push_back(actions[new_state]);
-					new_state = parent[new_state];
+				// Reconstruct the path of actions that led to the final state
+				while (*new_state < init_state) {
+					solution.push_back(actions.at(*new_state));
+					new_state = parent[*new_state];
 				}
 				return solution;
 			}
-			// If the key is not present, find returns an iterator to end
-			if (parent.find(new_state) == parent.end()) {
-				q.push(new_state);
-				parent[new_state] = current_state;
-				actions[new_state] = action;
-			}
+
 		}
 	}
 	return {};
