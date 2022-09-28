8097df6159a11bcbe14d73ef03c18943de614d9d
Elena Carasec <elena.carasec@gmail.com>
2022-09-28 18:08:39 +0200

    DFS: first version

--- sui-solution.cc
+++ sui-solution.cc
@@ -2,6 +2,7 @@
 #include <queue>
 #include <memory>
 #include <algorithm> 
+#include <stack>
 
 
 std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_state) {
@@ -42,6 +43,45 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
 }
 
 std::vector<SearchAction> DepthFirstSearch::solve(const SearchState &init_state) {
+	std::stack<std::shared_ptr<SearchState>> s;
+	std::map<SearchState, std::shared_ptr<SearchState>> parent;
+	std::map<SearchState, SearchAction> actions;
+	std::map<SearchState, int> depth;
+	int new_depth;
+
+	std::shared_ptr<SearchState> shared_init_state = std::make_shared<SearchState>(init_state);
+	s.push(shared_init_state);
+
+	while (!s.empty()) {
+		std::shared_ptr<SearchState> current_state = s.top();
+		s.pop();
+
+		if (depth[*current_state] <= depth_limit_) {
+			new_depth = depth[*current_state]++;
+
+			for (auto action : current_state->actions()) {
+				std::shared_ptr<SearchState> new_state = std::make_shared<SearchState>(action.execute(*current_state));
+
+				if (parent.find(*new_state) == parent.end()) {
+					s.push(new_state);
+					parent[*new_state] = current_state;
+					actions.insert(std::pair<SearchState, SearchAction>(*new_state, action));
+					depth.insert(std::pair<SearchState, int>(*new_state, new_depth));
+				}
+
+				if (new_state->isFinal()) {
+					std::vector<SearchAction> solution;
+					// Reconstruct the path of actions that led to the final state
+					while (*new_state < init_state) {
+						solution.push_back(actions.at(*new_state));
+						new_state = parent[*new_state];
+					}
+					return solution;
+				}
+			}
+		}
+	}
+
 	return {};
 }
 
