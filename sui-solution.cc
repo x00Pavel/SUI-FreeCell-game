a12d940cc303d019b771ccb7fe568a5681726a77
Elena Carasec <elena.carasec@gmail.com>
2022-10-10 23:01:53 +0200

    A_star: Add solve() implementation

--- sui-solution.cc
+++ sui-solution.cc
@@ -1,5 +1,7 @@
 #include "search-strategies.h"
 #include <queue>
+#include <set>
+#include <limits>
 #include <memory>
 #include <algorithm> 
 #include <stack>
@@ -101,5 +103,61 @@ double StudentHeuristic::distanceLowerBound(const GameState &state) const {
 }
 
 std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
+	std::set<std::shared_ptr<SearchState>> open;
+	struct node_info {
+		std::shared_ptr<SearchAction> action;
+		std::shared_ptr<SearchState> parent;
+		int depth;
+	};
+	std::map<std::shared_ptr<SearchState>, struct node_info> info;
+	std::set<SearchState> closed;
+	std::shared_ptr<SearchState> shared_init_state = std::make_shared<SearchState>(init_state);
+
+	open.insert(shared_init_state);
+	info[shared_init_state].parent = std::make_shared<SearchState>(init_state);
+	info[shared_init_state].action = nullptr;
+	info[shared_init_state].depth = 0;
+	
+	while (!open.empty()) {
+		int best = std::numeric_limits<int>::max();
+		std::shared_ptr<SearchState> best_choice = nullptr;
+		for (std::shared_ptr<SearchState> state_ptr: open) {
+			int h = compute_heuristic(*state_ptr, *heuristic_);
+			int g = info[state_ptr].depth;
+			// std::cout << "BEST: " << best << "\nh(x)+g(x): " << h + g << std::endl << std::endl;
+			if (h + g < best) {
+				best = h + g;
+				best_choice = state_ptr;
+			}
+		}
+
+		open.erase(best_choice);
+		closed.insert(*best_choice);
+		int new_depth = info[best_choice].depth + 1;
+
+		for (auto action : best_choice->actions()) {
+			std::shared_ptr<SearchState> new_state = std::make_shared<SearchState>(action.execute(*best_choice));
+			// If the key is not present, find returns an iterator to end
+			if (closed.find(*new_state) == closed.end()) {
+				open.insert(new_state);
+					struct node_info n_info = {
+						std::make_shared<SearchAction>(action),
+						best_choice,
+						new_depth
+					};
+					info.insert(std::pair<std::shared_ptr<SearchState>, node_info>(new_state, n_info));
+			}
+			
+			if (new_state->isFinal()) {
+				std::vector<SearchAction> solution;
+				// Reconstruct the path of actions that led to the final state
+				while (new_state != shared_init_state) {
+					solution.push_back(*(info.at(new_state).action));
+					new_state = info[new_state].parent;
+				}
+				return solution;
+			}
+		}
+	}
 	return {};
 }
